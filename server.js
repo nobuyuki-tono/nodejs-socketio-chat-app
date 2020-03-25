156ce4e7b1136901b1d138dc49494ed7b66d523f
Nobu <rockmusic2090@gmail.com>
2020-03-25 16:11:54 -0700

    connected to socket.io

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Run when client connects
+io.on("connection", socket => {
+  console.log("New WS Connection...");
+
+  socket.emit("message", "Welcome to ChatCord");
+
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joind the chat");
+
+  // Runs when client disconnects
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
