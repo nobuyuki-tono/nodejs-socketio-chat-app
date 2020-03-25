6c00945e46be70b41940b2b71d1ade5724950323
Nobu <rockmusic2090@gmail.com>
2020-03-25 16:20:49 -0700

    Added emit for sending message to server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Run when client connects
 io.on("connection", socket => {
-  console.log("New WS Connection...");
+  //console.log("New WS Connection...");
 
   socket.emit("message", "Welcome to ChatCord");
 
@@ -20,6 +20,9 @@ io.on("connection", socket => {
   socket.broadcast.emit("message", "A user has joind the chat");
 
   // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left chat room");
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
