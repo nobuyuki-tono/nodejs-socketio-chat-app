37507a78c9e02a39df4537baefd697615cf093c8
Nobu <rockmusic2090@gmail.com>
2020-03-26 21:34:20 -0700

    Created userLeave function

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ io.on("connection", socket => {
 
   // Listetn for chatMessage
   socket.on("chatMessage", msg => {
-    io.emit("message", formatMessage("USER", msg));
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
   // Runs when client disconnects
