d65053ec1d1f7e5660c9dbba99cb33ddacf196d3
Nobu <rockmusic2090@gmail.com>
2020-03-26 21:56:56 -0700

    Fixed issue

--- public/js/main.js
+++ public/js/main.js
@@ -66,6 +66,6 @@ function outputRoomName(room) {
 // Add users to DOM
 function outputUsers(users) {
   userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join()}
+    ${users.map(user => `<li>${user.username}</li>`).join("")}
   `;
 }
